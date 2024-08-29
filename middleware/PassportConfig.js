40d70e36a54dfd7777ae0e5afd19d541c972594e
Kasun Fernando <fernandokasun672@gmail.com>
2024-08-29 12:35:42 +0530

    updated

---
+++ middleware/PassportConfig.js
@@ -0,0 +1,23 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const UserSchema = require('../model/UserSchema');
+const crypto = require('crypto');
+
+
+passport.use(new GoogleStrategy({
+    clientId:process.env.CLIENT_ID,
+    clientSecret:process.env.CLIENT_SECRET,
+    callbackUrl:'/auth/google/callback',
+    passReqToCallBack:true
+}, async  (req, accessToken, refreshToken, profile, done) =>{
+    console.log(profile);
+    done(null,profile);
+}));
+
+passport.serializeUser((user,done) => {
+    done(null,user);
+})
+
+passport.deserializeUser(async (id,done) => {
+    done(null,id);
+})
