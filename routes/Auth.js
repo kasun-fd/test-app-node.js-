b99f4f31dd0eea37c0bf549edfc08e2cc3a0debb
Kasun Fernando <fernandokasun672@gmail.com>
2024-08-29 12:44:33 +0530

    routes

--- routes/Auth.js
+++ routes/Auth.js
@@ -2,5 +2,16 @@ const express = require('express');
 const passport = require('passport');
 const router = express.Router();
 
-router.get('/google', passport.authenticate());
+router.get('/google', passport.authenticate('google',{
+    scope:['profile','email']
+}));
+
+router.get('/logout', (req,resp)=>{
+    req.logout();
+    resp.redirect('/')
+});
+
+router.get('/google/callback', passport.authenticate('google'),(req,resp)=>{
+    resp.redirect('profile');
+});
 module.exports = router;
